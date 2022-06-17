993495fd91e1db6e6e22ea73a09aab39d6c92d17
gs2022 <81512075+sg20180546@users.noreply.github.com>
2022-06-17 23:16:58 +0900

    Delete .hello_module.ko.cmd

--- module/.hello_module.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/gs201/Desktop/kernel_study/module/hello_module.ko := ld -r  -m elf_x86_64  --build-id=sha1  -T scripts/module.lds -o /home/gs201/Desktop/kernel_study/module/hello_module.ko /home/gs201/Desktop/kernel_study/module/hello_module.o /home/gs201/Desktop/kernel_study/module/hello_module.mod.o;  true
