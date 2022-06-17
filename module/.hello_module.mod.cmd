bf68832650a49117bdbf23d73bd8f0291f64d7dd
gs2022 <81512075+sg20180546@users.noreply.github.com>
2022-06-17 23:17:04 +0900

    Delete .hello_module.mod.cmd

--- module/.hello_module.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/gs201/Desktop/kernel_study/module/hello_module.mod := { echo  /home/gs201/Desktop/kernel_study/module/hello_module.o;  echo; } > /home/gs201/Desktop/kernel_study/module/hello_module.mod
